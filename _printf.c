f432d09694b55a2d44f6104b48699bdab9fc3a87
MUHAMMED JAMI'U AGARAWU <92211740+jamiukabir22@users.noreply.github.com>
2022-08-10 13:16:38 +0100

    Printf project

--- _printf.c
+++ _printf.c
@@ -1,8 +1,8 @@
-#include <stdio.h>
-#include <stdlib.h>
-#include <unistd.h>
-#include "main.h"
 #include <stddef.h>
+#include "main.h"
+#include <unistd.h>
+#include <stdlib.h>
+#include <stdio.h>
 /**
  * printIdentifiers - prints special characters
  * @next: character after the %
@@ -24,9 +24,10 @@ int printIdentifiers(char next, va_list arg)
 		{"x", print_hex},
 		{"X", print_HEX},
 		{"S", print_STR},
-		{NULL, NULL},
+		{NULL, NULL}
 	};
-	for (functsIndex = 0; functs[functsIndex].indentifier =! NULL; functsIndex++)
+
+	for (functsIndex = 0; functs[functsIndex].indentifier != NULL; functsIndex++)
 	{
 		if (functs[functsIndex].indentifier[0] == next)
 			return (functs[functsIndex].printer(arg));
@@ -45,13 +46,14 @@ int printIdentifiers(char next, va_list arg)
 
 int _printf(const char *format, ...)
 {
-	int identifierPrinted = 0, charPrinted = 0;
 	unsigned int i;
+	int identifierPrinted = 0, charPrinted = 0;
 	va_list arg;
 
 	va_start(arg, format);
 	if (format == NULL)
 		return (-1);
+
 	for (i = 0; format[i] != '\0'; i++)
 	{
 		if (format[i] != '%')
